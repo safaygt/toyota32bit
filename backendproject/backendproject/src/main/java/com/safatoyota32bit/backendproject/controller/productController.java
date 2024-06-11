ac336cba7f29a409ec3dde75fcd03c45efe3ca4d
safa <safayigit40@gmail.com>
2024-06-11 15:32:34 +0300

    Logging with log4j2 has been started and also logging has been integrated to product controller.

--- backendproject/backendproject/src/main/java/com/safatoyota32bit/backendproject/controller/productController.java
+++ backendproject/backendproject/src/main/java/com/safatoyota32bit/backendproject/controller/productController.java
@@ -3,16 +3,20 @@ package com.safatoyota32bit.backendproject.controller;
 import com.safatoyota32bit.backendproject.dto.productDTO;
 import com.safatoyota32bit.backendproject.service.productService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/product")
+@Log4j2
 
 public class productController {
 
@@ -20,7 +24,23 @@ public class productController {
 
     @GetMapping
     public ResponseEntity<List<productDTO>> getAllProducts(){
-        List<productDTO> products = ProductService.getAllProducts();
+
+
+        log.info("Fetching all products");
+
+
+        List<productDTO> products;
+
+        try{
+            products = ProductService.getAllProducts();
+            log.debug("Number of products fetched: {}", products.size());
+
+        }catch (Exception e){
+            log.error("Error occurred while fetching products", e);
+            return ResponseEntity.status(500).build();
+
+        }
+        log.info("Successfully fetched all products");
         return ResponseEntity.ok(products);
 
     }
